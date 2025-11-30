d608934340a573f0120262eca3ab5b7819ad93ca
VRG <konuskov92@gmail.com>
2025-12-01 00:35:25 +0600

    week 7

---
+++ Assets/Scripts/UnitParameters.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class UnitParameters : MonoBehaviour
+{
+    [field: SerializeField] public float speed { get; private set; } = 4f;
+    [field: SerializeField] public float modelRadius { get; private set; } = 1f;
+    [field: SerializeField] public float startChaseDistance { get; private set; } = 5f;
+    [field: SerializeField] public float stoptChaseDistance { get; private set; } = 7f;
+
+    public float startAttackDistance { get { return modelRadius + _startAttackDistance; } }
+
+    public float stopAttackDistance { get { return modelRadius + _stopAttackDistance; } }
+
+    [SerializeField] public float _startAttackDistance = 1f;
+    [SerializeField] public float _stopAttackDistance = 1.5f;
+}
